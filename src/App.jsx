c722ca486796cc8cac5f601605fe5877d81c06b0
Anvarbek <c1221sharp@gmail.com>
2025-06-11 17:27:51 +0500

    feat: Monts is ready to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import styled from "styled-components";
+
+import "./App.css";
+import { BrowserRouter } from "react-router";
+import MainRoutes from "./routes/MainRouter";
+
+const Wrapper = styled.div`
+  width: 100%;
+`;
 
 function App() {
-
   return (
-    <>
-    icjsao
-    </>
-  )
+    <Wrapper>
+      <BrowserRouter>
+        <MainRoutes />
+      </BrowserRouter>
+    </Wrapper>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/MonthCard.jsx
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 356px;
+  /* background-color: ${({ bg }) => bg || "#f5f5f5"}; */
+  border-radius: 20px;
+  /* padding-bottom: 10px; */
+`;
+
+const Header = styled.div`
+  background-color: ${({ bg }) => bg || "#f5f5f5"};
+  padding: 16px 0;
+  border-radius: 20px 20px 0 0;
+`;
+
+const Title = styled.p`
+  font-size: 28px;
+  font-weight: 700;
+  text-align: center;
+  margin: 0;
+`;
+
+const Div = styled.div`
+  width: 100%;
+  height: 235px;
+  background-color: #fff;
+`
+
+const Week = styled.div`
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  text-align: center;
+  padding: 10px 5px;
+  background-color: #fff;
+`;
+
+const Day = styled.div`
+  font-weight: bold;
+  font-size: 14px;
+`;
+
+const DateCell = styled.div`
+  font-size: 14px;
+  padding: 6px 0;
+`;
+
+const MonthCard = ({ name, bgColor, startFrom, days }) => {
+  const daysOfWeek = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
+
+  const dateArray = Array(startFrom).fill("");
+  for (let i = 1; i <= days; i++) {
+    dateArray.push(i);
+  }
+
+  return (
+    <Wrapper bg={bgColor}>
+      <Header bg={bgColor}>
+        <Title>{name}</Title>
+      </Header>
+
+      <Div>
+        <Week>
+          {daysOfWeek.map((day) => (
+            <Day key={day}>{day}</Day>
+          ))}
+        </Week>
+
+        <Week>
+          {dateArray.map((date, idx) => (
+            <DateCell key={idx}>{date}</DateCell>
+          ))}
+        </Week>
+      </Div>
+    </Wrapper>
+  );
+};
+
+export default MonthCard;
